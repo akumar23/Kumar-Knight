b83443ccea75610bd04a21ec7315e8f6857266c5
Aryan Kumar <kumar.aryan@gmail.com>
2022-09-08 15:20:55 -0700

    added camera that moves with player

--- level.py
+++ level.py
@@ -11,7 +11,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #set up sprites for obstacles
-        self.visable_sprite = pygame.sprite.Group()
+        self.visable_sprite = YSortCameraGroup()
         self.obstacle_sprite = pygame.sprite.Group()
         self.create_map()
 
@@ -26,6 +26,22 @@ class Level:
                     self.player = Player((x,y), [self.visable_sprite], self.obstacle_sprite)
 
     def run(self):
-        self.visable_sprite.draw(self.display_surface)
+        self.visable_sprite.custom_draw(self.player)
         self.visable_sprite.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self) -> None:
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0]//2
+        self.half_height = self.display_surface.get_size()[1]//2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
